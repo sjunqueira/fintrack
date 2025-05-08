42524ad6053a71bec24ed8e0bf81bc75cec4065a
Sergio Junqueira <sergiojunqueira.s@gmail.com>
2025-05-08 19:44:34 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { Avatar, AvatarFallback, AvatarImage } from "./_components/ui/avatar";
+
 export default function Home() {
   return (
-    <div className="">
-      <h1>Teste</h1>
+    <div className="align flex justify-center font-bold text-blue-950">
+      <div className="flex flex-row gap-5">
+        <Avatar>
+          <AvatarImage src="https://github.com/sjunqueira.png" />
+          <AvatarFallback>SJ</AvatarFallback>
+        </Avatar>
+      </div>
     </div>
   );
 }

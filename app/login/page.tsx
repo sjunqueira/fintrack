3c264ccce119b3b6868b29dc6015be38ad6d9143
Sergio Junqueira <sergiojunqueira.s@gmail.com>
2025-05-08 20:34:54 -0300

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { Button } from "../_components/ui/button";
+import { LogIn } from "lucide-react";
+
+const LoginPage = () => {
+  return (
+    <div className="grid h-full grid-cols-2">
+      {/*Esquerda*/}
+      <div className="flex h-full max-w-[800px] flex-col justify-center p-24">
+        <div className="b-32 mx-auto rounded-2xl bg-neutral-900 p-8">
+          <Image
+            src={"/logo.svg"}
+            alt="finance.ai"
+            width={173}
+            height={39}
+            className="mb-8"
+          ></Image>
+          <h1 className="mb-8 text-4xl font-bold">Bem-vindo(a)</h1>
+          <p className="text-muted-foreground mb-3">
+            A Finance AI é uma plataforma de gestão financeira que utiliza IA
+            para monitorar suas movimentações, e oferecer insights
+            personalizados, facilitando o controle do seu orçamento.
+          </p>
+          <Button variant={"outline"} className="h-12 w-full">
+            <LogIn className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </div>
+      </div>
+      {/*Direita*/}
+      <div className="relative h-full w-full">
+        <Image
+          src="/login.png"
+          alt="Faça login"
+          fill
+          className="object-cover"
+        ></Image>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { Avatar, AvatarFallback, AvatarImage } from "./_components/ui/avatar";
 export default function Home() {
   return (
     <div className="align flex justify-center font-bold text-blue-950">
+      {/*ESquerda*/}
+      {/*Direita*/}
       <div className="flex flex-row gap-5">
         <Avatar>
           <AvatarImage src="https://github.com/sjunqueira.png" />

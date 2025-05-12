07aac84403eb1783466acdeffa08a91c785342b6
Sergio Junqueira <sergiojunqueira.s@gmail.com>
2025-05-11 21:51:47 -0300

    delete 4 files and update 1 file

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import { SessionProvider } from "next-auth/react";
 import Dashboard from "../../_dashboard/page";
+import Header from "../../_components/header";
 
 export default function Administrator() {
   return (
     <SessionProvider>
+      <Header />
       <Dashboard />
     </SessionProvider>
   );
---
+++ src/app/(pages)/transactions/page.tsx
@@ -0,0 +1,5 @@
+const TransactionsPage = () => {
+  return <p>Teste</p>;
+};
+
+export default TransactionsPage;
